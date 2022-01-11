f0f1e214f941a8c22e95e85617a0131cbaf5a5b1
tkyzmina <tkyzmina@gmail.com>
2022-01-11 11:56:37 +0300

    feat: add test solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = Math.abs(n).toString();
+    let newNum = "";
+    for (let i = 0; i < str.length; i++) {
+        newNum += str[str.length - (i + 1)];
+    }
+    return newNum;
+};
